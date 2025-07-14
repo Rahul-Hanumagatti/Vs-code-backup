25c5b786c0d951241034e90d1e11d3b556f1769a
Rahul H Hanumagatti <rahulhh999@gmail.com>
2025-07-14 19:50:36 +0530

    update

---
+++ python/function.py
@@ -0,0 +1,7 @@
+# define fun_name(parameters) :
+ #   // do something
+
+def print_sum(a,b=30):  # function defination , with default parameter
+    print(a+b)
+
+print_sum(1,2)
---
+++ python/practice.py
@@ -0,0 +1,4 @@
+name = input("Enter name ")
+print(len(name))
+print(name.find("@"))
+print(name.count("@"))
